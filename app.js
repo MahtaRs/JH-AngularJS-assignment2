c5aa49003882a3756ae3ee8f3c8211ecaaf13137
MahtaRs <55111151+MahtaRs@users.noreply.github.com>
2020-10-13 14:16:37 +0330

    Final Structure!

--- app.js
+++ app.js
@@ -1,33 +1,53 @@
 (function(){
   "use strict";
   angular.module("myapp" , [])
-  .controller("maincontroller" , doit);
-  var tobuy = [
-    {
-      name: "cookies", quantity: 10
-    },
-    {
-      name: "oreos", quantity: 8
-    },
-    {
-      name: "marshmello", quantity: 5
-    },
-    {
-      name: "icecreames", quantity: 3
-    },
-    {
-      name: "milkbottles", quantity: 12
-    }
-  ];
-  function doit ($scope){
-    $scope.to_buy_array = tobuy;
-    $scope.bought_items = [];
-    console.log($scope.to_buy_array);
-    $scope.buyitem = function (index){
-      $scope.bought_items.push($scope.to_buy_array[index]);
-      $scope.to_buy_array.splice(index, 1);
-      cosole.log($scope.bought_items);
+  .controller("ToBuyController" , ToBuyController)
+  .controller("AlreadyBoughtController" , AlreadyBoughtController)
+  .service('ShoppingListCheckOffService' , ShoppingListCheckOffService);
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService){
+    var ItemAdd = this;
+    ItemAdd.to_buy_array = ShoppingListCheckOffService.getItems();
+    ItemAdd.buyitem = function(index){
+      ShoppingListCheckOffService.buyItem(index);
+  };
 
+  }
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController (ShoppingListCheckOffService){
+    var ItemShow = this;
+    ItemShow.bought_items = ShoppingListCheckOffService.ShowBoughtItems();
+
+  }
+  function ShoppingListCheckOffService (){
+    var service = this;
+    var to_buy = [
+      {
+        name: "cookies", quantity: 10
+      },
+      {
+        name: "oreos", quantity: 8
+      },
+      {
+        name: "marshmello", quantity: 5
+      },
+      {
+        name: "icecreames", quantity: 3
+      },
+      {
+        name: "milkbottles", quantity: 12
+      }
+    ];
+    var bought_items = [];
+    service.ShowBoughtItems = function (){
+      return bought_items;
+    }
+    service.getItems = function (){
+      return to_buy;
+    };
+    service.buyItem = function (index){
+      bought_items.push(to_buy[index]);
+      to_buy.splice(index, 1);
     };
   }
 })();
